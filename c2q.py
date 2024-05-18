32d3e47cea18c7d9ab47cc7b99014242b58e97e6
GINTOAHC <chaotingchen10@gmail.com>
2024-05-18 16:27:47 +0800

    refactor: Replace ipynb with html and pure python

---
+++ c2q.py
@@ -0,0 +1,442 @@
+# %%
+import time
+import os
+import copy
+
+# PyTorch
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torchvision
+from torchvision import datasets, transforms
+
+# Pennylane
+import pennylane as qml
+from pennylane import numpy as np
+
+torch.manual_seed(42)
+np.random.seed(42)
+
+# Plotting
+import matplotlib.pyplot as plt
+
+# Enable SSL
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+# %%
+# OpenMP: number of parallel threads.
+os.environ["OMP_NUM_THREADS"] = "1"
+
+# %%
+# filtered_classes = ['bear', 'tiger']
+# n_qubits = 4                # Number of qubits
+# step = 0.0004               # Learning rate
+# batch_size = 4              # Number of samples for each training step
+# num_epochs = 1          # Number of training epochs
+# q_depth = 6                 # Depth of the quantum circuit (number of variational layers)
+# gamma_lr_scheduler = 0.1    # Learning rate reduction applied every 10 epochs.
+# q_delta = 0.01              # Initial spread of random quantum weights
+# start_time = time.time()    # Start of the computation timer
+n_qubits = 4  # Number of qubits
+step = 0.0006  # Learning rate
+batch_size = 4  # Number of samples for each training step
+num_epochs = 10  # Number of training epochs
+q_depth = 7  # Depth of the quantum circuit (number of variational layers)
+gamma_lr_scheduler = 0.1  # Learning rate reduction applied every 10 epochs.
+q_delta = 0.01  # Initial spread of random quantum weights
+
+# %%
+dev = qml.device("default.qubit", wires=n_qubits)
+device = torch.device("cpu")
+
+# %%
+data_transforms = {
+    "train": transforms.Compose(
+        [
+            # transforms.RandomResizedCrop(224),     # uncomment for data augmentation
+            # transforms.RandomHorizontalFlip(),     # uncomment for data augmentation
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            # Normalize input channels using mean values and standard deviations of ImageNet.
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    ),
+    "val": transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    ),
+}
+
+trainset = torchvision.datasets.CIFAR100(
+    root="./data/", train=True, download=True, transform=data_transforms["train"]
+)
+testset = torchvision.datasets.CIFAR100(
+    root="./data/", train=False, download=True, transform=data_transforms["val"]
+)
+image_datasets_full = {"train": trainset, "val": testset}
+
+# CIFAR classes
+class_names = ("bear", "tiger")
+
+# Get indices of samples associated to filtered_classes
+filtered_labels = [3, 88]
+sub_indices = {"train": [], "val": []}
+for phase in ["train", "val"]:
+    for idx, label in enumerate(image_datasets_full[phase].targets):
+        if label in filtered_labels:
+            sub_indices[phase].append(idx)
+
+# Initialize sub-datasets according to filtered indices
+image_datasets = {
+    x: torch.utils.data.Subset(image_datasets_full[x], sub_indices[x])
+    for x in ["train", "val"]
+}
+
+
+def labels_to_filtered(labels):
+    """Maps CIFAR labels (3,88) to the index of filtered_labels"""
+    return [filtered_labels.index(label) for label in labels]
+
+
+# =================== end CIFAR dataset loading ==========================
+
+# Number of samples
+dataset_sizes = {x: len(image_datasets[x]) for x in ["train", "val"]}
+
+# Initialize dataloader
+dataloaders = {
+    x: torch.utils.data.DataLoader(
+        image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=0
+    )
+    for x in ["train", "val"]
+}
+
+
+# function to plot images
+def imshow(inp, title=None):
+    """Display image from tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    # Inverse of the initial normalization operation.
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+
+
+# %%
+# Get a batch of training data
+inputs, classes = next(iter(dataloaders["val"]))
+
+# Make a grid from batch
+out = torchvision.utils.make_grid(inputs)
+
+imshow(out, title=[class_names[filtered_labels.index(x)] for x in classes])
+
+plt.show()
+
+# %%
+dataloaders = {
+    x: torch.utils.data.DataLoader(
+        image_datasets[x], batch_size=batch_size, shuffle=True
+    )
+    for x in ["train", "val"]
+}
+
+
+# %%
+def H_layer(nqubits):
+    """Layer of single-qubit Hadamard gates."""
+    for idx in range(nqubits):
+        qml.Hadamard(wires=idx)
+
+
+def RY_layer(w):
+    """Layer of parametrized qubit rotations around the y axis."""
+    for idx, element in enumerate(w):
+        qml.RY(element, wires=idx)
+
+
+def entangling_layer(nqubits):
+    """Layer of CNOTs followed by another shifted layer of CNOT."""
+    # In other words it should apply something like :
+    # CNOT  CNOT  CNOT  CNOT...  CNOT
+    #   CNOT  CNOT  CNOT...  CNOT
+    for i in range(0, nqubits - 1, 2):  # Loop over even indices: i=0,2,...N-2
+        qml.CNOT(wires=[i, i + 1])
+    for i in range(1, nqubits - 1, 2):  # Loop over odd indices:  i=1,3,...N-3
+        qml.CNOT(wires=[i, i + 1])
+
+
+def H_layer(nqubits):
+    """Layer of single-qubit Hadamard gates."""
+    for idx in range(nqubits):
+        qml.Hadamard(wires=idx)
+
+
+def RY_layer(w):
+    """Layer of parametrized qubit rotations around the y axis."""
+    for idx, element in enumerate(w):
+        qml.RY(element, wires=idx)
+
+
+def entangling_layer(nqubits):
+    """Layer of CNOTs followed by another shifted layer of CNOT."""
+    # In other words it should apply something like :
+    # CNOT  CNOT  CNOT  CNOT...  CNOT
+    #   CNOT  CNOT  CNOT...  CNOT
+    for i in range(0, nqubits - 1, 2):  # Loop over even indices: i=0,2,...N-2
+        qml.CNOT(wires=[i, i + 1])
+    for i in range(1, nqubits - 1, 2):  # Loop over odd indices:  i=1,3,...N-3
+        qml.CNOT(wires=[i, i + 1])
+
+
+# %%
+@qml.qnode(dev)
+def quantum_net(q_input_features, q_weights_flat):
+    """
+    The variational quantum circuit.
+    """
+
+    # Reshape weights
+    q_weights = q_weights_flat.reshape(q_depth, n_qubits)
+
+    # Start from state |+> , unbiased w.r.t. |0> and |1>
+    H_layer(n_qubits)
+
+    # Embed features in the quantum node
+    RY_layer(q_input_features)
+
+    # Sequence of trainable variational layers
+    for k in range(q_depth):
+        entangling_layer(n_qubits)
+        RY_layer(q_weights[k])
+
+    # Expectation values in the Z basis
+    exp_vals = [qml.expval(qml.PauliZ(position)) for position in range(n_qubits)]
+    return tuple(exp_vals)
+
+
+# %%
+class DressedQuantumNet(nn.Module):
+    """
+    Torch module implementing the *dressed* quantum net.
+    """
+
+    def __init__(self):
+        """
+        Definition of the *dressed* layout.
+        """
+
+        super().__init__()
+        self.pre_net = nn.Linear(512, n_qubits)
+        self.q_params = nn.Parameter(q_delta * torch.randn(q_depth * n_qubits))
+        self.post_net = nn.Linear(n_qubits, 2)
+
+    def forward(self, input_features):
+        """
+        Defining how tensors are supposed to move through the *dressed* quantum
+        net.
+        """
+
+        # obtain the input features for the quantum circuit
+        # by reducing the feature dimension from 512 to 4
+        pre_out = self.pre_net(input_features)
+        q_in = torch.tanh(pre_out) * np.pi / 2.0
+
+        # Apply the quantum circuit to each element of the batch and append to q_out
+        q_out = torch.Tensor(0, n_qubits)
+        q_out = q_out.to(device)
+        for elem in q_in:
+            q_out_elem = (
+                torch.hstack(quantum_net(elem, self.q_params)).float().unsqueeze(0)
+            )
+            q_out = torch.cat((q_out, q_out_elem))
+
+        # return the two-dimensional prediction from the postprocessing layer
+        return self.post_net(q_out)
+
+
+# %%
+weights = torchvision.models.ResNet18_Weights.IMAGENET1K_V1
+model_hybrid = torchvision.models.resnet18(weights=weights)
+
+for param in model_hybrid.parameters():
+    param.requires_grad = False
+
+
+# Notice that model_hybrid.fc is the last layer of ResNet18
+model_hybrid.fc = DressedQuantumNet()
+
+# Use CUDA or CPU according to the "device" object.
+model_hybrid = model_hybrid.to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer_hybrid = optim.Adam(model_hybrid.fc.parameters(), lr=step)
+
+exp_lr_scheduler = lr_scheduler.StepLR(
+    optimizer_hybrid, step_size=10, gamma=gamma_lr_scheduler
+)
+
+
+# %%
+def train_model(model, criterion, optimizer, scheduler, num_epochs):
+    since = time.time()
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+    best_loss = 10000.0  # Large arbitrary number
+    best_acc_train = 0.0
+    best_loss_train = 10000.0  # Large arbitrary number
+    print("Training started:")
+
+    for epoch in range(num_epochs):
+
+        # Each epoch has a training and validation phase
+        for phase in ["train", "val"]:
+            if phase == "train":
+                # Set model to training mode
+                model.train()
+            else:
+                # Set model to evaluate mode
+                model.eval()
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            n_batches = dataset_sizes[phase] // batch_size
+            it = 0
+            for inputs, labels in dataloaders[phase]:
+                since_batch = time.time()
+                batch_size_ = len(inputs)
+                inputs = inputs.to(device)
+                labels = torch.tensor(labels_to_filtered(labels))
+                labels = labels.to(device)
+                optimizer.zero_grad()
+
+                # Track/compute gradient and make an optimization step only when training
+                with torch.set_grad_enabled(phase == "train"):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+                    if phase == "train":
+                        loss.backward()
+                        optimizer.step()
+
+                # Print iteration results
+                running_loss += loss.item() * batch_size_
+                batch_corrects = torch.sum(preds == labels.data).item()
+                running_corrects += batch_corrects
+                print(
+                    "Phase: {} Epoch: {}/{} Iter: {}/{} Batch time: {:.4f}".format(
+                        phase,
+                        epoch + 1,
+                        num_epochs,
+                        it + 1,
+                        n_batches + 1,
+                        time.time() - since_batch,
+                    ),
+                    end="\r",
+                    flush=True,
+                )
+                it += 1
+
+            # Print epoch results
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects / dataset_sizes[phase]
+            print(
+                "Phase: {} Epoch: {}/{} Loss: {:.4f} Acc: {:.4f}        ".format(
+                    "train" if phase == "train" else "val  ",
+                    epoch + 1,
+                    num_epochs,
+                    epoch_loss,
+                    epoch_acc,
+                )
+            )
+
+            # Check if this is the best model wrt previous epochs
+            if phase == "val" and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == "valn" and epoch_loss < best_loss:
+                best_loss = epoch_loss
+            if phase == "train" and epoch_acc > best_acc_train:
+                best_acc_train = epoch_acc
+            if phase == "train" and epoch_loss < best_loss_train:
+                best_loss_train = epoch_loss
+
+            # Update learning rate
+            if phase == "train":
+                scheduler.step()
+
+    # Print final results
+    model.load_state_dict(best_model_wts)
+    time_elapsed = time.time() - since
+    print(
+        "Training completed in {:.0f}m {:.0f}s".format(
+            time_elapsed // 60, time_elapsed % 60
+        )
+    )
+    print(
+        "Best test loss: {:.4f} | Best test accuracy: {:.4f}".format(
+            best_loss, best_acc
+        )
+    )
+    return model
+
+
+model_hybrid = train_model(
+    model_hybrid, criterion, optimizer_hybrid, exp_lr_scheduler, num_epochs=num_epochs
+)
+
+
+# %%
+def visualize_model(model, num_images=12, fig_name="Predictions"):
+    images_so_far = 0
+    fig = plt.figure(fig_name)
+    # fig,ax = plt.subplots(3, 4,figsize=(14,9))
+
+    model.eval()
+    correct = 0
+    wrong = 0
+    with torch.no_grad():
+        # fig,ax = plt.subplots(3, 4,figsize=(14,9))
+        for _i, (inputs, labels) in enumerate(dataloaders["val"]):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            for j in range(inputs.size()[0]):
+                # ax[i//4][i%4].imshow(preds[[i]])
+                print(preds[[j]], labels[[j]])
+                if (preds[[j]] == 0 and labels[[j]] == 3) or (
+                    preds[[j]] == 1 and labels[[j]] == 88
+                ):
+                    correct += 1
+                else:
+                    wrong += 1
+
+                images_so_far += 1
+                ax = plt.subplot(num_images // 2, 4, images_so_far)
+                ax.axis("off")
+                ax.set_title("[{}]".format(class_names[preds[j]]))
+                imshow(inputs.cpu().data[j])
+                if images_so_far == num_images:
+                    print("Correct images {}, Wrong images {}".format(correct, wrong))
+                    return
+
+
+batch_size = 12
+# visualize_model(model_hybrid, num_images=batch_size)
+visualize_model(model_hybrid)
+
+# %%
